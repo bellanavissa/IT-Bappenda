a991318511b41278bc4a95711f35b9b64691adab
Giakom080818 <sugeng.wahono26@gmail.com>
2025-06-23 15:51:28 +0800

    u

--- inc/koneksi.php
+++ inc/koneksi.php
@@ -12,7 +12,7 @@ if ($koneksi ==  false) {
 		$host2 = "202.46.154.45";
 		$user2 = "202.46.154.45";
 		$pass2 = "minum";
-		$database2 = "it_bappedan";
+		$database2 = "it_bappenda";
 
 
 		$koneksi = mysqli_connect($host2,$user2,$pass2,$database2);
@@ -23,9 +23,7 @@ if ($koneksi ==  false) {
 			echo"berhasil koneksi online offline";
 		}
 	
-} else {
-    echo "berhasil koneksi offline";
-}
+} 
 
 
  ?>
