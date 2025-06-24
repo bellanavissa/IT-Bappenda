a44c558fd80524005c38ae36f702ad40afb0c85b
Giakom080818 <sugeng.wahono26@gmail.com>
2025-06-24 10:21:51 +0800

    uu

--- inc/xml/api_wp.php
+++ inc/xml/api_wp.php
@@ -1,1 +1,29 @@
+<?php
+include 'inc/koneksi.php';
+
+header("Content-Type: text/xml");
+
+$query = "SELECT * FROM tbl_wp";
+$result = mysqli_query($conn, $query);
+
+echo "<?xml version='1.0' encoding='UTF-8'?>\n";
+echo "<wp_list>\n";
+
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "  <wajib pajak>\n";
+        echo "    <id_wp>" . $row['id_wp'] . "</id_wp>\n";
+        echo "    <nama>" . htmlspecialchars($row['nama']) . "</nama>\n";
+        echo "    <alamat>" . htmlspecialchars($row['alamat']) . "</alamat>\n";
+        echo "    <no_ktp>" . htmlspecialchars($row['no_ktp']) . "</no_ktp>\n";
+        echo "    <no_hp>" . $row['no_hp'] . "</no_hp>\n";
+    
+        echo "  </wp>\n";
+    }
+} else {
+    echo "  <message>Tidak ada data</message>\n";
+}
+
+echo "</wp_list>";
+?>
 
