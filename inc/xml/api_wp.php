73674b0c47163a9c9fbd32f0942bdd672a2abb59
Giakom080818 <sugeng.wahono26@gmail.com>
2025-06-24 11:01:04 +0800

    uu

--- inc/xml/api_wp.php
+++ inc/xml/api_wp.php
@@ -1,5 +1,10 @@
 <?php
-include 'inc/koneksi.php';
+    $host2 = "202.46.154.45";
+    $user2 = "202.46.154.45";
+    $pass2 = "minum";
+    $database2 = "it_bappenda";
+
+    $koneksi = mysqli_connect($host2,$user2,$pass2,$database2);
 
 header("Content-Type: text/xml");
 
@@ -11,14 +16,13 @@ echo "<wp_list>\n";
 
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
-        echo "  <wajib pajak>\n";
+        echo "  <wajib_pajak>\n";
         echo "    <id_wp>" . $row['id_wp'] . "</id_wp>\n";
         echo "    <nama>" . htmlspecialchars($row['nama']) . "</nama>\n";
         echo "    <alamat>" . htmlspecialchars($row['alamat']) . "</alamat>\n";
         echo "    <no_ktp>" . htmlspecialchars($row['no_ktp']) . "</no_ktp>\n";
-        echo "    <no_hp>" . $row['no_hp'] . "</no_hp>\n";
-    
-        echo "  </wp>\n";
+        echo "    <no_hp>" . htmlspecialchars($row['no_hp']) . "</no_hp>\n";
+        echo "  </wajib_pajak>\n";
     }
 } else {
     echo "  <message>Tidak ada data</message>\n";
@@ -26,4 +30,3 @@ if (mysqli_num_rows($result) > 0) {
 
 echo "</wp_list>";
 ?>
-
